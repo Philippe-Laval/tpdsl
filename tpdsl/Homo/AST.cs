9a6923ad1cd9b7117b671bf04d86335df4070d01
Philippe Laval <philippe.laval.1968@laposte.net>
2022-07-16 15:02:37 +0200

    first version

---
+++ tpdsl/Homo/AST.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/***
+ * Excerpted from "Language Implementation Patterns",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/tpdsl for more book information.
+***/
+using System.Threading.Tasks;
+
+namespace Homo
+{
+    /// <summary>
+    /// Homogeneous AST node type
+    /// </summary>
+    public class AST
+    {
+        /// <summary>
+        /// From which token did we create node?
+        /// </summary>
+        public Token? Token { get; set; }
+        /// <summary>
+        /// Normalized list of children
+        /// </summary>
+        public List<AST>? Children { get; set; }
+
+        /// <summary>
+        /// For making nil-rooted nodes
+        /// </summary>
+        public AST() 
+        { 
+        } 
+        
+        public AST(Token t) 
+        {
+            Token = t; 
+        }
+
+        /// <summary>
+        /// Create node from token type; used mainly for imaginary tokens 
+        /// </summary>
+        /// <param name="tokenType"></param>
+        public AST(int tokenType) { this.Token = new Token(tokenType); }
+
+        /// <summary>
+        /// External visitors need unique int per node for id purposes while walking.
+        /// </summary>
+        /// <returns></returns>
+        public int GetNodeType() { return Token?.Type ?? Token.INVALID_TOKEN_TYPE; }
+
+        /// <summary>
+        /// Add as kid
+        /// </summary>
+        /// <param name="t"></param>
+        public void AddChild(AST t)
+        {
+            if (Children == null) Children = new List<AST>();
+            Children.Add(t);
+        }
+
+        public bool IsNil() { return Token == null; }
+
+        /// <summary>
+        /// Compute string for single node
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() 
+        { 
+            return Token?.ToString() ?? string.Empty;
+        }
+
+        /// <summary>
+        /// Compute string for a whole tree not just a node
+        /// </summary>
+        /// <returns></returns>
+        public string ToStringTree()
+        {
+            if (Children == null || Children.Count() == 0) return this.ToString();
+
+            StringBuilder buf = new StringBuilder();
+            
+            if (!IsNil())
+            {
+                buf.Append("(");
+                buf.Append(this.ToString());
+                buf.Append(' ');
+            }
+
+            for (int i = 0; Children != null && i < Children.Count(); i++)
+            {
+                AST t = (AST)Children[i]; // normalized (unnamed) children
+                if (i > 0) buf.Append(' ');
+                buf.Append(t.ToStringTree());
+            }
+
+            if (!IsNil()) buf.Append(")");
+            
+            return buf.ToString();
+        }
+    }
+}
+
---
+++ tpdsl/Homo/Program.cs
@@ -0,0 +1,24 @@
+﻿/***
+ * Excerpted from "Language Implementation Patterns",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/tpdsl for more book information.
+***/
+
+using Homo;
+
+Token plus = new Token(Token.PLUS, "+");
+Token one = new Token(Token.INT, "1");
+Token two = new Token(Token.INT, "2");
+AST root = new AST(plus);
+root.AddChild(new AST(one));
+root.AddChild(new AST(two));
+Console.WriteLine("1+2 tree: " + root.ToStringTree());
+
+AST list = new AST(); // make nil node as root for a list
+list.AddChild(new AST(one));
+list.AddChild(new AST(two));
+Console.WriteLine("1 and 2 in list: " + list.ToStringTree());
+  
