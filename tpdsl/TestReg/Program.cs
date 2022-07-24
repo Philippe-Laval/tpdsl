e673f21b22de7d9b170b43b9fbb52626b3098c57
Philippe Laval <philippe.laval.1968@laposte.net>
2022-07-24 14:38:35 +0200

    add proper examples

--- tpdsl/TestReg/Program.cs
+++ tpdsl/TestReg/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+/*
+    <run class="Interpreter" argline="t.rcode -trace -dis -dump" />
+
+    <echo message="Now >> t2.rcode" />
+    <run class="Interpreter" argline="t2.rcode -trace -dis -dump" />
+
+    <echo message="Now >> t3.rcode" />
+    <run class="Interpreter" argline="t3.rcode -trace -dis -dump" />
+
+    <echo message="Now >> t4.rcode" />
+    <run class="Interpreter" argline="t4.rcode -trace -dis -dump" />
+
+    <echo message="Now >> struct.rcode" />
+    <run class="Interpreter" argline="struct.rcode -trace -dis -dump" />
+
+<!--
+  Takes long, so turned off.
+    <echo message="Now >> loop.rcode" />
+    <run class="Interpreter" argline="loop.rcode -trace -dis -dump" />
+--> 
+ */
