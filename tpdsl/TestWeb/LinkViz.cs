83d6d2b4c8b0709ec4a9f8bbd3391bf81d35af39
Philippe Laval <philippe.laval.1968@laposte.net>
2022-07-30 18:35:43 +0200

    Add new projects

---
+++ tpdsl/TestWeb/LinkViz.cs
@@ -0,0 +1,68 @@
+﻿using System;
+/***
+ * Excerpted from "Language Implementation Patterns",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/tpdsl for more book information.
+***/
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://www.stringtemplate.org/
+// https://github.com/antlr/antlrcs
+
+using Antlr4.StringTemplate;
+
+namespace TestWeb
+{
+    public class Link
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+
+        public Link(String from, String to)
+        {
+            From = from;
+            To = to;
+        }
+    }
+
+    public class LinkViz
+    {
+        private TemplateGroup _templates;
+
+        private List<Link> _links = new List<Link>();
+
+
+        public LinkViz()
+        {
+            string directory = Directory.GetCurrentDirectory();
+            string path = Path.Combine(directory, "DOT.stg");
+
+            _templates = new TemplateGroupFile(path);
+        }
+
+        public void AddLink(String from, String to)
+        {
+            _links.Add(new Link(from, to));
+        }
+
+        public override string ToString()
+        {
+            Template fileST = _templates.GetInstanceOf("file");
+            fileST.Add("gname", "testgraph");
+            foreach (Link x in _links)
+            {
+                Template edgeST = _templates.GetInstanceOf("edge");
+                edgeST.Add("from", x.From);
+                edgeST.Add("to", x.To);
+                fileST.Add("edges", edgeST);
+            }
+            return fileST.Render(); // render (eval) template to text
+        }
+    }
+}
---
+++ tpdsl/TestWeb/Program.cs
@@ -0,0 +1,27 @@
+﻿/***
+ * Excerpted from "Language Implementation Patterns",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/tpdsl for more book information.
+***/
+namespace TestWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkViz viz = new LinkViz();
+            viz.AddLink("index.html", "login.html");
+            viz.AddLink("index.html", "about.html");
+            viz.AddLink("login.html", "error.html");
+            viz.AddLink("about.html", "news.html");
+            viz.AddLink("index.html", "news.html");
+            viz.AddLink("logout.html", "index.html");
+            viz.AddLink("index.html", "logout.html");
+            Console.WriteLine(viz.ToString());
+        }
+    }
+}
+
